87e803156a2d380479eefcf6abe879003ba9d779
Mokh <mokh223@gmail.com>
2020-12-15 09:54:52 +0800

    string

---
+++ string/index.js
@@ -0,0 +1,19 @@
+let str = "Salesforce";
+
+console.log(str);
+console.log(str.substr(0,5));
+console.log(str);
+console.log(str.substring(0,5));
+console.log(str);
+console.log(str.slice(0,5));
+console.log(str);
+
+console.log("test");
+
+console.log(str);
+console.log(str.substr(1,5));
+console.log(str);
+console.log(str.substring(1,5));
+console.log(str);
+console.log(str.slice(1,5));
+console.log(str);
